4c9b50f3dfa55d2fd717cf5c47e8e48b84410fb5
Craig <craig@cra.io>
2020-04-09 13:20:15 +0100

    almost

--- bspwm/.config/bspwm/bspwm_colors.sh
+++ bspwm/.config/bspwm/bspwm_colors.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-bspc config focused_border_color "#a71650"
-bspc config normal_border_color  "#730f37"
+bspc config focused_border_color "#6448a3"
+bspc config normal_border_color  "#453270"
